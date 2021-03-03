01a0e25a4aee4cdf9cab984e2bb6c8bbf40ffd10
Alberto Fecchi <alberto.f@luckyseven.it>
2021-03-04 00:26:43 +0100

    Avatar with different sources and elevation

--- lib/widgets/avatar.dart
+++ lib/widgets/avatar.dart
@@ -1,12 +1,14 @@
 part of 'package:avatars/avatars.dart';
 
 class Avatar extends StatefulWidget {
-
   final List<Source> sources;
-  final String facebookId;
-  final String gravatar;
+  final String name;
+  final String value;
 
-  Avatar({this.sources, this.facebookId, this.gravatar});
+  final double elevation;
+  final double radius;
+
+  Avatar({this.elevation = 0, this.name, this.radius = 50, this.sources, this.value});
 
   @override
   _AvatarState createState() => _AvatarState();
@@ -15,52 +17,55 @@ class Avatar extends StatefulWidget {
 
 class _AvatarState extends State<Avatar> {
 
-  Source _source;
-  AvatarData _avatarData;
+  bool _loading = true;
 
-  bool found = false;
+  Widget _avatar;
 
   @override
   void initState() {
-
+    _buildBestAvatar().then((a) => setState((){
+      _avatar = a;
+      _loading = false;
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return _bestSource();
+    if (_loading) {
+      return _loader();
+    }
+    return Material(
+      type: MaterialType.circle,
+      color: Colors.orange,
+      elevation: this.widget.elevation,
+      child: Container(
+        width: this.widget.radius * 2,
+        height: this.widget.radius * 2,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(this.widget.radius),
+          child:  _avatar,
+        ),
+      ),
+    );
   }
 
-  Widget _bestSource() {
-    if (found) {
-      // return CircleAvatar(
-      //   backgroundImage: CachedNetworkImageProvider(_avatarData.imageUrl),
-      //   backgroundColor: Colors.brown.shade800,
-      //   radius: 30,
-      //   onBackgroundImageError: (a, b) {
-      //     found = false;
-      //     setState(() {});
-      //   },
-      // );
-      return Image.memory(_avatarData.imageBytes);
-      // return Image(image: CachedNetworkImageProvider(_avatarData.imageUrl));
+  Future<Widget> _buildBestAvatar() async {
+    Image avatar;
+    if (this.widget.sources != null && this.widget.sources.length > 0) {
+      for (int i = 0; i < this.widget.sources.length; i++) {
+        avatar = await this.widget.sources.elementAt(i).getAvatar();
+        if (avatar != null) {
+          return avatar;
+        }
+      }
     }
-    if (this.widget.sources.length > 0) {
-      Source current = this.widget.sources.first;
-      this.widget.sources.removeAt(0);
-
-      print("PROVO UNO");
-
-      current.getAvatar(200).then((ad) {
-        _avatarData = ad;
-        found = true;
-        setState(() {});
-      });
+    if (this.widget.name != null) {
+      List<String> nameParts = this.widget.name.split(' ');
+      return Text(nameParts.map((p) => p.substring(0,1)).join('').substring(0,2));
     }
-    return CircleAvatar(
-      backgroundColor: Colors.brown.shade800,
-      child: _loader(),
-      radius: 30,
-    );
+    if (this.widget.value != null) {
+      return Text(this.widget.value);
+    }
   }
 
   Widget _loader() {
@@ -70,9 +75,26 @@ class _AvatarState extends State<Avatar> {
 }
 
 abstract class Source {
-  Future<AvatarData> getAvatar(int size);
+  bool _cached = false;
+  Image _cache;
 
-  Future<Uint8List> downloadImage(String url) {
+  String getAvatarUrl();
+
+  Future<Image> getAvatar() {
+    if (_cached) {
+      return Future.value(_cache);
+    }
+    return _getImageBytes(getAvatarUrl()).then((bytes) {
+      if (bytes != null) {
+        _cached = true;
+        _cache = Image.memory(bytes);
+        return _cache;
+      }
+      return null;
+    });
+  }
+
+  Future<Uint8List> _getImageBytes(String url) {
     Completer<Uint8List> completer = Completer();
     HttpClient client = new HttpClient();
     var _downloadData = List<int>();
@@ -81,12 +103,17 @@ abstract class Source {
           return request.close();
         })
         .then((HttpClientResponse response) {
-          response.listen((d) => _downloadData.addAll(d),
-          onDone: () {
-            completer.complete(Uint8List.fromList(_downloadData));
+          if (response.statusCode > 399 || !['image/jpeg', 'image/png'].contains(response.headers.contentType.toString())) {
+            completer.complete(null);
+          } else {
+            response.listen(
+              (d) => _downloadData.addAll(d),
+              onDone: () {
+                completer.complete(Uint8List.fromList(_downloadData));
+              }
+            );
           }
-      );
-    });
+        }).catchError((e) => completer.complete(null));
     return completer.future;
   }
 }
@@ -94,41 +121,42 @@ abstract class Source {
 class FacebookSource extends Source {
   String facebookId;
   String appToken; // Since Sep 2020 you need to use an App Token to get user's profile pictures
+  int size;
 
-  FacebookSource(this.facebookId, this.appToken);
+  FacebookSource(this.facebookId, this.appToken, [this.size = 500]);
 
   @override
-  Future<AvatarData> getAvatar(int size) {
-    return downloadImage('https://graphaaa.facebook.com/$facebookId/picture?width=$size&height=$size').then((bytes) => AvatarData(imageBytes: bytes));
-    return Future.value(AvatarData(imageUrl: 'https://graphaaa.facebook.com/$facebookId/picture?width=$size&height=$size'));
-  }
+  String getAvatarUrl() => 'https://graph.facebook.com/$facebookId/picture?width=$size&height=$size';
 
 }
 
 class GravatarSource extends Source {
   String identifier; // Mail or Hash
+  int size;
 
-  GravatarSource(this.identifier);
+  GravatarSource(this.identifier, [this.size = 500]);
 
   @override
-  Future<AvatarData> getAvatar(int size) {
+  String getAvatarUrl() {
     String hash = identifier;
     RegExp matcher = RegExp(r'^[a-f0-9]{32}$');
     if (!matcher.hasMatch(hash)) {
       hash = md5.convert(utf8.encode(identifier)).toString();
     }
-    return downloadImage('https://secure.gravatar.com/avatar/$hash?s=$size&d=404').then((bytes) => AvatarData(imageBytes: bytes));
-
-    return Future.value(AvatarData(imageUrl: 'https://secure.gravatar.com/avatar/$hash?s=$size&d=404'));
+    return 'https://secure.gravatar.com/avatar/$hash?s=$size&d=404';
   }
 
 }
 
-class AvatarData {
-  Uint8List imageBytes;
-  String imageUrl;
-  String name;
-  String value;
+class PlaceholderSource extends Source {
+  Image image; // Mail or Hash
 
-  AvatarData({this.imageBytes, this.imageUrl, this.name, this.value});
+  PlaceholderSource(this.image);
+
+  @override
+  String getAvatarUrl() => null;
+
+  @override
+  Future<Image> getAvatar() => Future.value(image);
+
 }
